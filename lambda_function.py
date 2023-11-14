20163356d949445ccac95a56527cbb50e77110c0
madZ604 <madhura.chatterjee8744@gmail.com>
2023-11-14 01:01:50 -0500

    TESTING101

--- lambda_function.py
+++ lambda_function.py
@@ -14,7 +14,7 @@ HOST = 'search-photos-xzh7hrqko56izsipclb6hdfng4.us-east-1.es.amazonaws.com'
 INDEX = 'photos' #domain name of OS
 SERVICE = 'es'
 
-# TEST 100
+# TEST 1002
 
 client = boto3.client('lexv2-runtime')
 
